fe18d16db7932e6d95cfa9513b05808e46a377ae
Logan Oram <logan.oram@keyin.com>
2025-02-25 13:54:19 -0330

    Entities added

--- src/main/java/com/plane/airport/Airport.java
+++ src/main/java/com/plane/airport/Airport.java
@@ -1,4 +1,55 @@
 package com.plane.airport;
 
+import com.plane.cities.Cities;
+import com.plane.aircraft.Aircraft;
+
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private Cities city;
+
+    @OneToMany(mappedBy = "departureAirport", cascade = CascadeType.ALL)
+    private List<Aircraft> departingAircrafts;
+
+    @OneToMany(mappedBy = "arrivalAirport", cascade = CascadeType.ALL)
+    private List<Aircraft> arrivingAircrafts;
+
+    public Airport() {}
+
+    public Airport(String name, String code, Cities city) {
+        this.name = name;
+        this.code = code;
+        this.city = city;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
+    public Cities getCity() { return city; }
+    public void setCity(Cities city) { this.city = city; }
+
+    public List<Aircraft> getDepartingAircrafts() { return departingAircrafts; }
+    public void setDepartingAircrafts(List<Aircraft> departingAircrafts) { this.departingAircrafts = departingAircrafts; }
+
+    public List<Aircraft> getArrivingAircrafts() { return arrivingAircrafts; }
+    public void setArrivingAircrafts(List<Aircraft> arrivingAircrafts) { this.arrivingAircrafts = arrivingAircrafts; }
 }
+
