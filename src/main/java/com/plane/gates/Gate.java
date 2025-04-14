20e9c6e7fc0eb1cc20a3cbbc0d5e0a47dbd0c8f1
Nicholas Sharpe <162132214+nsharpe1@users.noreply.github.com>
2025-04-14 11:06:26 -0230

    Finished and completed Gate.java

--- src/main/java/com/plane/gates/Gate.java
+++ src/main/java/com/plane/gates/Gate.java
@@ -1,4 +1,50 @@
 package com.plane.gates;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Gate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gateId;
+
+    private String terminal;
+    private String number;
+
+    public Gate() {
+
+    }
+
+    public Gate(String terminal, String number) {
+        this.terminal = terminal;
+        this.number = number;
+    }
+
+    public Long getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(Long gateId) {
+        this.gateId = gateId;
+    }
+
+    public String getTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(String terminal) {
+        this.terminal = terminal;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
