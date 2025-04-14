7b39091ccf887d4387f2dcd7508f5a0b501b0606
Nicholas Sharpe <162132214+nsharpe1@users.noreply.github.com>
2025-04-14 11:08:33 -0230

    Finished and completed GateRepository.java

--- src/main/java/com/plane/gates/GateRepository.java
+++ src/main/java/com/plane/gates/GateRepository.java
@@ -1,4 +1,9 @@
 package com.plane.gates;
 
-public interface GateRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface GateRepository extends CrudRepository<Gate, Long> {
+    public Gate findByTerminal(String terminal);
+
+    public Gate findByNumber(String number);
 }
