6e8f0369fff42919f545d5fa347fa81ce7c1f984
dderible <declan.derible@keyin.com>
2025-04-15 12:22:59 -0230

    Flight classes part 1

--- src/main/java/com/plane/flights/Flight.java
+++ src/main/java/com/plane/flights/Flight.java
@@ -1,17 +1,19 @@
 package com.plane.flights;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "flights")
 public class Flight {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long flightId;
 
-    // reminder to myself: flight seat number, destinations, airline.
+    private String flightSeat;
+    private String flightOrigin;
+    private String flightDestination;
+    private String flightAirline;
+
     // maybe?: switch airports function here?
 }
--- src/main/java/com/plane/flights/FlightRepository.java
+++ src/main/java/com/plane/flights/FlightRepository.java
@@ -1,1 +1,14 @@
 package com.plane.flights;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByFlightId(Long flightId);
+    List<Flight> findByFlightDestination(String flightDestination);
+    List<Flight> findByFlightOrigin(String flightOrigin);
+    List<Flight> findByFlightAirline(String flightAirline);
+}
